041a13d2752f4bd3678098138e14de8385d1d758
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2024-03-30 22:50:15 -0600

    Add function to compare strings with the edit distance method

--- standards/text_comparison.py
+++ standards/text_comparison.py
@@ -1,3 +1,6 @@
+"""Contains the functions to get the similarity among two different strings."""
+
+import editdistance
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -11,3 +14,11 @@ def cosine_similarity_compare(text1: str, text2: str) -> float:
     cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
 
     return cosine_sim[0][0]
+
+
+def edit_distance_compare(text1: str, text2: str) -> float:
+    """Compares two different strings using the edit distance method."""
+    edit_dist = editdistance.eval(text1, text2)
+    max_length = max(len(text1), len(text2))
+
+    return 1 - edit_dist / max_length
