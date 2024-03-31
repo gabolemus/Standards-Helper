35f909b34f38001b2ca3b264fd73ea7a439570c4
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2024-03-30 23:59:35 -0600

    Add general config file

---
+++ standards/config.py
@@ -0,0 +1,6 @@
+"""File with the configurations to run the project."""
+
+original_standards_file = "/mnt/c/Users/Gabo/Downloads/Comparison STD Gabo.xlsx"
+new_standards_file = "/mnt/c/Users/Gabo/Downloads/DraftUnifiedStandard_PlanningVersion1.0 Gabo.xlsx"
+
+original_standards_ws = ["RCS", "GRS", "RWS", "RMS", "RAS", "RDS"]
