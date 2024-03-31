4eca7de496c26389ab09b7fd192a29049bcda849
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2024-03-31 15:50:05 -0600

    Update config

--- standards/config.py
+++ standards/config.py
@@ -6,3 +6,8 @@ original_standards_file = "C:\\Users\\Gabo\\Downloads\\Comparison STD Gabo.xlsx"
 new_standards_file = "C:\\Users\\Gabo\\Downloads\\DraftUnifiedStandard_PlanningVersion1.0 Gabo.xlsx"
 
 original_standards_ws = ["RCS", "GRS", "RWS", "RMS", "RAS", "RDS"]
+
+
+def get_worksheet_index(worksheet_name: str) -> int:
+    """Get the index of the worksheet."""
+    return original_standards_ws.index(worksheet_name)
